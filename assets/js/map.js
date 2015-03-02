8cb7cdbb4c42bb9edc49c924347d2427ec05b3fc
Coleman McCormick <cmccormick@gmail.com>
2015-03-02 13:09:55 -0500

    Updates to mapping UI

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,29 +1,38 @@
-var clubSearch = [];
+var featureList, clubSearch = []
+
+data = "https://web.fulcrumapp.com/shares/82982e4c55707a34.geojson";
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiY29sZW1hbm0iLCJhIjoieW8wN2lTNCJ9.j1zlDeYFSVAl8XWjaHY-5w';
-var map = L.mapbox.map('map', 'colemanm.gga7agbd').setView([48, 13], 4);;
+var map = L.mapbox.map('map', 'colemanm.lbd40ge4').setView([48, 13], 4);;
 
-var clubs = L.geoJson(null, {
-  style: function (feature) {
-    return {
-      color: "red",
-      fill: false,
-      opacity: 1,
-      clickable: false
-    };
-  },
-  onEachFeature: function (feature, layer) {
-    clubSearch.push({
-      name: layer.feature.properties.name,
-      source: "Clubs",
-      id: L.stamp(layer)
-    });
-  }
-});
-$.getJSON("https://web.fulcrumapp.com/shares/82982e4c55707a34.geojson", function (data) {
-  clubs.addData(data);
-  clubs.addTo(map);
-});
+// var clubs = L.geoJson(null, {
+//   style: function (feature) {
+//     return {
+//       color: "red",
+//       fill: false,
+//       opacity: 1,
+//       clickable: false
+//     };
+//   },
+//   onEachFeature: function (feature, layer) {
+//     clubSearch.push({
+//       name: layer.feature.properties.name,
+//       source: "Clubs",
+//       id: L.stamp(layer)
+//     });
+//   }
+// });
+// $.getJSON(data, function (data) {
+//   clubs.addData(data);
+// });
+
+var featureLayer = L.mapbox.featureLayer(data)
+    .addTo(map);
+
+syncSidebar();
+
+var list = document.getElementById("feature-list");
+
 
 /* Highlight search box text on click */
 $("#searchbox").click(function () {
@@ -36,6 +45,27 @@ $("#searchbox").keypress(function (e) {
     e.preventDefault();
   }
 });
+
+function syncSidebar() {
+  /* Empty sidebar features */
+  $("#feature-list tbody").empty();
+  /* Loop through layer and add only features which are in the map bounds */
+  featureLayer.eachLayer(function (layer) {
+    if (map.hasLayer(featureLayer)) {
+      if (map.getBounds().contains(layer.getLatLng())) {
+        $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src=""></td><td class="feature-name">' + layer.feature.properties.NAME + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+      }
+    }
+  });
+
+  /* Update list.js featureList */
+  featureList = new List("features", {
+    valueNames: ["feature-name"]
+  });
+  // featureList.sort("feature-name", {
+  //   order: "asc"
+  // });
+}
 
 /* Typeahead search functionality */
 $(document).one("ajaxStop", function () {
